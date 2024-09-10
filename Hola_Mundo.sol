edaaf53c0c5f3a0e258859d47be08a1db52933d5
JuanDRamirez0217 <juanda.r0217@gmail.com>
2024-09-09 19:39:26 -0500

    Archivo "Hola_Mundo" agregado al respositorio y deploy del contrato

---
+++ Hola_Mundo.sol
@@ -0,0 +1,10 @@
+
+// Mi primer contrato Hola Mundo
+// SPDX-License-Identifier: MIT
+
+//0x962708Eb3eAb07BF89D8105Bf41ECB99F49DCa85
+pragma solidity ^0.8.24;
+
+contract hola_mundo {
+    string public saludo = "Hola mundo desde Avalanche Colombia";
+}
