82d94497e5b5d0bd793f6d3f4bf3c8d21ee17ba7
JuanDRamirez0217 <juanda.r0217@gmail.com>
2024-10-03 20:22:54 -0500

    Variables de S

---
+++ Solidity_vars.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Solidity_vars{
+    address public block_now;           //Ver la dirección de minero actual
+    uint public block_diff;             //Dificultad del bloque actual
+    uint public block_num;              //Muestra el número de bloque de la cadena actual
+    bytes32 public block_hash;          //Tipo de dato para el blockhash
+    uint public time_stamp;             //Muestra el tiempo actual en formato epoch
+    uint public gas_left;               //Gas restante
+    address public sender;              //Qiuen ejecuta el contrato
+    bytes4 public sig_id;               //Primeros 4 bytesd e la llamada de la función
+    uint public gas_limit;              //Límite de gas del bloque actual
+
+    function update_params() public {
+        block_now = block.coinbase;
+        block_diff = block.prevrandao;
+        block_num = block.number;
+        time_stamp = block.timestamp;
+        gas_left = gasleft();
+        sender = msg.sender;
+        sig_id = msg.sig;
+        gas_limit = block.gaslimit;
+
+    }
+}
