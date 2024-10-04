632d6da56b22481d2c1f8be87be3f80f187d54ed
JuanDRamirez0217 <juanda.r0217@gmail.com>
2024-10-03 23:07:28 -0500

    Condicional If pero implementando el constructor

--- portero_didactico.sol
+++ portero_didactico.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.24;
 contract portero_didactico{
     
     uint256 public numero; //Variable de estado que almacenará a la edad
+    uint256 public edad_init; //Edad límite para la mayoría de edad
+    constructor (uint256 _edad_init){
+        edad_init = _edad_init;
+    }
 
     function establecer_numero(uint256 _numero) public{
         numero = _numero;
@@ -12,7 +16,7 @@ contract portero_didactico{
     function mensaje() public view returns (string memory){
         
         //If-Else
-        if (numero >= 18){
+        if (numero >= edad_init){
             return "Puedes ingresar porque eres mayor de edad";
         }else{
             return "No puedes ingresar, debes ser mayor de edad";
